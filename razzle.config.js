4a2208fa45854e88aa00877d5b7ef6d708eec5ef
DimagaDev <lobodindimaga@gmail.com>
2023-07-06 12:51:48 +0300

    first 14

--- razzle.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    modify: (config, { target, dev }, webpack) => {
-        if (target === "node") {
-            config.externals = []
-        }
-        return config;
-    },
-};
